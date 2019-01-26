92aec43131becea4d540bd68301e220b8fcd53ba
Rob Anderson <rob@andrsn.uk>
2019-01-26 12:42:04 +0000

    :star: Removed use of casex and improve url encoding

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const casex = require('casex')
 const fs = require('fs')
 const { join } = require('path')
 
 const headTag = `<!-- toc-head -->`
 const tailTag = `<!-- toc-tail -->`
+
+const slugify = s => encodeURIComponent(s.toLowerCase().replace(/\s+/g, '-'))
 
 function tableOfContents(file, inline = false) {
   try {
@@ -27,7 +28,7 @@ function tableOfContents(file, inline = false) {
         headings.push({
           title,
           level: hashes.length,
-          handle: casex(title.toLowerCase(), 'ca-se')
+          handle: slugify(title)
         })
       }
 
@@ -40,7 +41,7 @@ function tableOfContents(file, inline = false) {
     headings.forEach(({ title, handle, level }) => {
       let item = ''
       while (item.length / 2 < level - 2) item += '  '
-      item += `* [${title}](#${handle})`
+      item += `- [${title}](#${handle})`
       outputLines.push(item)
     })
 
